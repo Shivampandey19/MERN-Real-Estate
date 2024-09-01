92c399f3f900e65966ca382b7e0cddd7fd7d0c84
Shivam <shivamkpandey1910@gmail.com>
2024-09-02 01:02:55 +0530

    Complete Sign In page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -47,14 +47,30 @@ export default function SignUp() {
         Sign Up
       </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type='text' placeholder='userName' 
-        className='border p-3 rounded-lg' id='userName' onChange={handleChange} /> 
-        <input type='email' placeholder='email' 
-        className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type='password' placeholder='password' 
-        className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3
-        rounded-lg uppercase hover:opacity-95 
+        <input 
+        type='text' 
+        placeholder='username' 
+        className='border p-3 rounded-lg' 
+        id='userName' onChange={handleChange} 
+        />
+        <input 
+        type='email' 
+        placeholder='email' 
+        className='border p-3 rounded-lg' 
+        id='email' onChange={handleChange} 
+        />
+        <input 
+        type='password' 
+        placeholder='password' 
+        className='border p-3 rounded-lg' 
+        id='password' onChange={handleChange} 
+        />
+        <button 
+        disabled={loading} 
+        className='bg-slate-700 
+        text-white p-3
+        rounded-lg uppercase 
+        hover:opacity-95 
         disabled:opacity-80'>
           {loading ? 'Loading...': 'Sign Up'}
         </button>
