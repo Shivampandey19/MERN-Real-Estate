44e27f128b641dec1b4c21b8dad2f6593df8566f
Shivam <shivamkpandey1910@gmail.com>
2024-09-05 00:58:59 +0530

    Complete show User Listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,26 @@
 import { useSelector } from 'react-redux';
-import { useRef, useState,useEffect } from 'react';
+import { useRef, 
+  useState,
+  useEffect 
+} from 'react';
 import { app } from '../Firebase';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSucess, deleteUserFailure, signOutUserStart, signOutUserFailure, signOutUserSucess } from '../Redux/User/userSlice.js';
+import { 
+  getDownloadURL, 
+  getStorage, 
+  ref, 
+  uploadBytesResumable 
+} from 'firebase/storage';
+import { 
+  updateUserStart, 
+  updateUserSuccess, 
+  updateUserFailure, 
+  deleteUserStart, 
+  deleteUserSucess, 
+  deleteUserFailure, 
+  signOutUserStart, 
+  signOutUserFailure, 
+  signOutUserSucess
+} from '../Redux/User/userSlice.js';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -12,8 +30,10 @@ export default function Profile() {
   const [file,setFile] = useState(undefined);
   const [filePercentage ,setFilePercentage] = useState(0);
   const [fileUploadError, setFileuploaderror] = useState(false);
-  const [formData, setFormData] =useState({});
-  const [updateSuccess, setUpdateSuccess]= useState(false);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings ,setUserListings]=useState([]);
   const dispatch = useDispatch();
   
   
@@ -115,6 +135,22 @@ export default function Profile() {
     } catch (error) {
       dispatch(signOutUserFailure(data.message));
     }
+  };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.sucess === false){
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+    
   }
 
   return (
@@ -209,6 +245,44 @@ export default function Profile() {
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully' : '' }
       </p>
+      <button onClick={handleShowListings} 
+      className='text-green-700 w-full'>
+        Show Listing
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+      {userListings && 
+      userListings.length > 0 &&
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl'></h1>
+        {userListings.map((listing) => (
+        <div key={listing._id} 
+        className='border rounded-lg p-3 flex
+        justify-between items-center gap-4'>
+          <Link to={`/listing/${listing._id}`}>
+            <img src={listing.imageUrls} alt='listing cover' 
+            className='h-16 w-16 object-contain'
+            />
+          </Link>
+          <Link className='text-slate-700 font-semibold 
+            hover:underline truncate flex-1' 
+            to={`/listing/${listing._id}`}>
+            <p>
+              {listing.name}
+            </p>
+          </Link>
+          <div className='flex flex-col items-center'>
+            <button className='text-red-700 uppercase'>
+              Delete
+            </button>
+            <button className='text-green-700 uppercase'>
+              Edit
+            </button>
+          </div>
+        </div>
+      ))}
+      </div>}
     </div>
   )
 }
